ee09fff0c18bee49c616574ad56afea98a784c52
caora <biffi92@googlemail.com>
2016-03-13 22:52:14 +0100

    Continued: Java Anagrams

--- Java/Cycle1/06_Anagrams/src/Anagrams.java
+++ Java/Cycle1/06_Anagrams/src/Anagrams.java
@@ -4,5 +4,26 @@
 
 public class Anagrams {
 
+    public static String[] getMutations(String word) {
+        String mutations[] = new String[getNumberMutations(word)];
 
+        for(int i = 0; i < word.length(); i++) {
+            if (i == 0) {
+                mutations[i] = word;
+            } else {
+                mutations[i] = "" + word.charAt(1) + word.charAt(0);
+            }
+        }
+
+        return mutations;
+    }
+
+    private static int getNumberMutations(String word) {
+        // number mutations equals word-length factorial
+        int result = 1;
+        for (int i = word.length(); i > 1; i--) {
+            result *= i;
+        }
+        return result;
+    }
 }
--- Java/Cycle1/06_Anagrams/src/AnagramsTest.java
+++ Java/Cycle1/06_Anagrams/src/AnagramsTest.java
@@ -6,4 +6,14 @@ import static org.junit.Assert.*;
  */
 public class AnagramsTest {
 
+    @Test
+    public void testCreateMutations_oneLetterWords() {
+        assertArrayEquals("test1", new String[] {"a"}, Anagrams.getMutations("a"));
+    }
+
+    @Test
+    public void testCreateMutations_twoLetterWords() {
+        assertArrayEquals("test1", new String[] {"ab", "ba"}, Anagrams.getMutations("ab"));
+        assertArrayEquals("test2", new String[] {"cd", "dc"}, Anagrams.getMutations("cd"));
+    }
 }
