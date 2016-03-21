cd374ffb064e0ca4bc138b3ffc4adfc57fe7b10b
caora <biffi92@googlemail.com>
2016-03-21 23:10:23 +0100

    Finished: Java WordChains

--- Java/Cycle1/19_WordChains/src/WordChains.java
+++ Java/Cycle1/19_WordChains/src/WordChains.java
@@ -1,21 +1,148 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
- * Created by Lizzie on 14.03.2016.
+ * Created by Liz on 14.03.2016.
  */
 public class WordChains {
 
-    public static String[] chain(String word1, String word2) {
+    private String[] dictionary;
 
-        if (isValid(word1) && isValid(word2)) {
+    public WordChains(String filepath) {
+        dictionary = loadDictionary(filepath);
+    }
 
-            return new String[]{word1, word2};
+    public String[] chain(String word1, String word2) {
+        word1 = clean(word1);
+        word2 = clean(word2);
+        if(validateInput(word1, word2)) {
+            return getChain(word1, word2);
         }
         return null;
     }
 
-    private static boolean isValid (String word) {
-        if(word == "abc" || word == "def") {
+    private String[] getChain(String word1, String word2) {
+
+        if(word1.equals(word2)) {
+            return new String[]{word1};
+        }
+
+        String[] subChain = getSubChain(word1, word2);
+        if(subChain == null) {
+            return null;
+        }
+
+        return join(word1, subChain);
+    }
+
+    private String[] getSubChain(String word1, String word2) {
+        for(int i = 0; i < word1.length(); i++) {
+            if(word1.charAt(i) != word2.charAt(i)) {
+                String newWord = createNewWord(word1, word2, i);
+                String[] subChain = chain(newWord, word2);
+                if(subChain != null) {
+                    return subChain;
+                }
+            }
+        }
+        String[] words = getAlternativeWords(word1, word2);
+        if(words != null) {
+            for (int i = 0; i < words.length; i++) {
+                String[] chain = chain(words[i], word2);
+                if (chain != null) {
+                    return chain;
+                }
+            }
+        }
+        return null;
+    }
+
+    private String createNewWord(String word1, String word2, int pos) {
+        return word1.substring(0,pos) + word2.charAt(pos) + word1.substring(pos+1);
+    }
+
+    private String createNewWord(String word1, char character, int pos) {
+        return word1.substring(0, pos) + character + word1.substring(pos + 1);
+    }
+
+    private String[] getAlternativeWords(String word1, String word2) {
+        ArrayList<String> alternatives = new ArrayList<String>();
+        for(int i = 0; i < word1.length(); i++) {
+            for(int j = 'a'; j <='z'; j++) {
+                String newWord = createNewWord(word1, (char)j, i);
+                if(searchDictionary(newWord) != -1) {
+                    alternatives.add(newWord);
+                };
+            }
+        }
+        return toArray(alternatives);
+    }
+
+    private boolean validateInput(String word1, String word2) {
+        if (word1 == null || word2 == null || word1.length() != word2.length()) {
             return false;
         }
         return true;
+    }
+
+    private String[] join(String firstWord, String[] rest) {
+        String[] result = new String[rest.length + 1];
+        result[0] = firstWord;
+        for(int j = 0; j < rest.length; j++) {
+            result[j+1] = rest[j];
+        }
+        return result;
+    }
+
+    protected String clean (String word) {
+        word = word.toLowerCase();
+
+        if(searchDictionary(word) == -1) {
+            return null;
+        }
+        return word;
+    }
+
+    protected int searchDictionary(String word) {
+        for (int i = 0; i < dictionary.length; i++) {
+            if(dictionary[i].equals(word)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private String[] loadDictionary(String filepath) {
+        String[] lines = null;
+        try(BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            lines = readLines(br);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+
+    private String[] readLines(BufferedReader br) throws IOException {
+        ArrayList<String> lines = new ArrayList<String>();
+        String line = br.readLine();
+
+        while (line != null) {
+            lines.add(line);
+            line = br.readLine();
+        }
+
+        return toArray(lines);
+    }
+
+    private String[] toArray(ArrayList<String> list) {
+        String[] array = new String[list.size()];
+        array = list.toArray(array);
+        return array;
+    }
+
+    public String[] getDictionary() {
+        return dictionary;
     }
 }
--- Java/Cycle1/19_WordChains/src/WordChainsTest.java
+++ Java/Cycle1/19_WordChains/src/WordChainsTest.java
@@ -2,18 +2,57 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by Lizzie on 14.03.2016.
+ * Created by Liz on 14.03.2016.
  */
 public class WordChainsTest {
 
+    String[] testDictionary = {"bear", "beer", "cat", "cot", "dog", "dot", "hog", "lead", "log", "rat", "read", "reed", "ruby"};
+    String filepath = "miniwordlist.txt";
+    
+    WordChains wordChainer1 = new WordChains(filepath);
+
     @Test
-    public void chainInvalidWord() {
-        assertArrayEquals("test1", null, WordChains.chain("abc", "def"));
+    public void loadDictionary() {
+        String[] dictionary = wordChainer1.getDictionary();
+        assertEquals("length of dictionary should be 13", 13, dictionary.length);
+        assertEquals("word at pos 7 should be 'lead'", "lead", dictionary[7]);
+        assertEquals("word at pos 12 should be 'ruby'", "ruby", dictionary[12]);
+    }
+
+    @Test
+    public void searchDictionary() {
+        assertEquals(-1, wordChainer1.searchDictionary("beelzebub"));
+        assertEquals(2, wordChainer1.searchDictionary("cat"));
+        assertEquals(4, wordChainer1.searchDictionary("dog"));
+    }
+
+    @Test
+    public void chainInvalidWords() {
+        assertNull("words not in dictionary are invalid", wordChainer1.chain("asdfaf", "wookie"));
+        assertNull("words not the same length are invalid", wordChainer1.chain("lead", "log"));
     }
 
     @Test
     public void chainLengthOne() {
-        assertArrayEquals("test1", new String[]{"dog", "hog"}, WordChains.chain("dog", "hog"));
-        assertArrayEquals("test2", new String[]{"bear", "beer"}, WordChains.chain("bear", "beer"));
+        assertArrayEquals("test identical words", new String[]{"ruby"}, wordChainer1.chain("ruby", "ruby"));
+        assertArrayEquals("test uppercase words", new String[]{"cat"}, wordChainer1.chain("cat", "CAT"));
+    }
+
+    @Test
+    public void chainLengthTwo() {
+        assertArrayEquals("test1", new String[]{"dog", "hog"}, wordChainer1.chain("dog", "hog"));
+        assertArrayEquals("test2", new String[]{"bear", "beer"}, wordChainer1.chain("bear", "beer"));
+        assertArrayEquals("test2", new String[]{"dot", "cot"}, wordChainer1.chain("dot", "cot"));
+    }
+
+    @Test
+    public void chainLengthThree() {
+        assertArrayEquals("test1", new String[] {"dog", "dot", "cot"}, wordChainer1.chain("dog", "cot"));
+        assertArrayEquals("test2", new String[] {"lead", "read", "reed"}, wordChainer1.chain("lead", "reed"));
+    }
+
+    @Test
+    public void chainLengthFive() {
+       assertArrayEquals("test longer chain", new String[] {"dog", "dot", "cot", "cat", "rat"}, wordChainer1.chain("dog", "rat"));
     }
 }
